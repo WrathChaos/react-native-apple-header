8c84e2b9df359e226624fe0feb0fe189ef98ac04
FreakyCoder <kurayogun@gmail.com>
2020-08-22 21:59:57 +0300

    AppleHeader is published and tested

--- example/App.js
+++ example/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView, StatusBar } from "react-native";
-import AppleHeader from "./build/dist/AppleHeader";
+import AppleHeader from "react-native-apple-header";
 // ? Assets
 const profileImageSource =
   "https://images.unsplash.com/photo-1504730030853-eff311f57d3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
