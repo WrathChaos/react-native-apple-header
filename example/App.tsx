9ce2981f99b9735c1cb4d6715f6117aad756b23e
FreakyCoder <kurayogun@gmail.com>
2023-02-04 14:33:04 +0300

    feat: version 1.0.0 is here with code cleaning

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView, StatusBar } from "react-native";
-import AppleHeader from "react-native-apple-header";
+import AppleHeader from "./lib/AppleHeader";
 // ? Assets
 const profileImageSource =
   "https://images.unsplash.com/photo-1504730030853-eff311f57d3c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80";
@@ -14,6 +14,9 @@ const App = () => {
           imageSource={{
             uri: profileImageSource,
           }}
+          dateTitle={"Monday, 27 November"}
+          largeTitle={"For You"}
+          onPress={() => {}}
         />
       </SafeAreaView>
     </>
